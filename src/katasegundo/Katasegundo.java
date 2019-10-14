01cf06167d6738d1d18ce3acd73938cea475ac95
Fran J Sánchez G <Fran J Snchez G@DESKTOP-284IO7D>
2019-10-14 18:23:25 +0200

    kata2 versión 3

--- src/katasegundo/Katasegundo.java
+++ src/katasegundo/Katasegundo.java
@@ -7,19 +7,11 @@ public class Katasegundo {
 
     public static void main(String[] args) {
         int data[] = {2, 5, 5, 9, 8, 12, 5, 9, 4, 12, 6, 5, 6, 7, 2};
-        Map <Integer, Integer> histogram = new HashMap<>();
-        for (int i = 0; i < data.length; i++) {
-            if (histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            } else {
-                histogram.put(data[i], 1);
-            }
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) +1 : 1);
-        }
-        for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
-            System.out.println(entry.getKey()+ "==>" + entry.getValue());
-        }
+            Histogram histo = new Histogram(data);
+            Map<Integer, Integer> histgr = histo.getHistogram();
+            
+        for(Map.Entry<Integer, Integer> entry : histgr.entrySet()) {
+        System.out.println(entry.getKey()+ "==>" + entry.getValue());
         }
     }
 }
